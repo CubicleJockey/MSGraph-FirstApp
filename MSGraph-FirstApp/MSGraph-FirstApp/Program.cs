f540e07dd70b5485d54562fd1ce78093dd1bd6c3
adavis <cubicle.jockey@live.com>
2020-03-04 14:29:20 -0800

    Creating the initial .NET Core Console App
    Creating the initial app for trying MS Graph in .NET Core

---
+++ MSGraph-FirstApp/MSGraph-FirstApp/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using static System.Console;
+
+namespace MSGraph_FirstApp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            WriteLine($".NET Core Graph First App{Environment.NewLine}");
+
+
+            var isValidChoice = int.TryParse(ReadLine(), out int choice);
+            if(!isValidChoice)
+            {
+                //Invalid options choosing one
+                choice = 1;
+            }
+
+            var message = choice switch
+            {
+                0 => "Goodbye...",
+                1 => "<Place Holder for Access Token",
+                2 => "<Place Holder for Calendar List",
+                _ => "Invalid Choice!, Please Try Again"
+            };
+
+            WriteLine(message);
+        }
+    }
+}
